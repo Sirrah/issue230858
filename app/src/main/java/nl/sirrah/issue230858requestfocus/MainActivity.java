ac5b73bd88f43f5096d270ee0dcd66af230c874f
Peter Oost <peteroost@sirrah.nl>
2017-01-04 13:21:44 +0100

    Place input inside fragment, this fails

--- app/src/main/java/nl/sirrah/issue230858requestfocus/MainActivity.java
+++ app/src/main/java/nl/sirrah/issue230858requestfocus/MainActivity.java
@@ -1,7 +1,7 @@
 package nl.sirrah.issue230858requestfocus;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +9,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragment, new EditFragment())
+                .commit();
     }
 }
